e1c328abc57544a069bb37c544426d5203dcbd53
Кирилл Ямщиков <yamshchikov_k@mail.ru>
2022-05-06 19:07:16 +0300

    Add completed task 3

---
+++ SEM4-LR0/task3.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int main() {
+    setlocale(LC_ALL, "rus");
+
+    std::string string;
+    std::cout << "Введите строку: ";
+    std::cin >> string;
+
+    // проверить если нет букв 'o'
+    if (string.find("o") == std::string::npos) {
+        std::cout << "Ошибка: в слове нет букв \'o\'\n";
+        return 1;
+    }
+
+    // найти кол-во всех букв в слове
+    size_t letters_count = string.size();
+
+    size_t pos = 0;
+    while ((pos = string.find("o", pos)) != std::string::npos) {
+        string.insert(pos, " ");
+        string.insert(pos + 2, " ");
+
+        pos += 2;
+    }
+
+    std::cout << "Строка: \"" << string << "\"\n";
+    std::cout << "Кол-во всех букв: " << letters_count << "\n";
+
+    return 0;
+}
