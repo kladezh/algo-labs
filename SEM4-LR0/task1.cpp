3b2517bddc00f45dc489eeeb70ed7d1ff995be75
Кирилл Ямщиков <yamshchikov_k@mail.ru>
2022-05-06 18:01:37 +0300

    Add completed task 1

---
+++ SEM4-LR0/task1.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+bool do_divide(const int divisible, const int divider) {
+    return (divisible % divider == 0);
+}
+
+bool is_simple(const int number) {
+    for (int i = 2; i < number; i++) {
+        if (do_divide(number, i)) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int main() {
+    setlocale(LC_ALL, "rus");
+
+    int number;
+    std::cout << "Введите натуральное число, большее единицы: ";
+    std::cin >> number;
+
+    if (number < 1) {
+        std::cout << "Ошибка: число должно быть больше единицы...";
+        return 1;
+    }
+
+    // найти наибольший простой делитель
+    int max_simple_div = 1;
+    for (int x = 1; x <= number / 2; x++) {
+        if (is_simple(x) && do_divide(number, x)) {
+            max_simple_div = x;
+        }
+    }
+
+    std::cout << "Наибольшой простой делитель числа (" << number << "): " << max_simple_div << "\n";
+
+    return 0;
+}
