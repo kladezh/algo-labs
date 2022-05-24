df03d693ffa4713a6aace54db205d9a9119d6259
Кирилл Ямщиков <yamshchikov_k@mail.ru>
2022-05-24 17:01:14 +0300

    Add console array operations to lab 3 from lab 2.

--- SEM4-LR3/Program.cs
+++ SEM4-LR3/Program.cs
@@ -9,7 +9,37 @@ namespace SEM4_LR3
     internal class Program
     {
         static void Main(string[] args)
+        { 
+            int[] array = ConsoleInitArray();
+
+            Console.WriteLine("\nСгенерированный массив:");
+            ConsoleWriteArray(array);
+        }
+
+        static int[] ConsoleInitArray()
         {
+            Console.Write("Введите размер массива: ");
+            int arrayLength = int.Parse(Console.ReadLine());
+
+            int[] array = new int[arrayLength];
+
+            Random random = new Random();
+            for (int i = 0; i < arrayLength; i++)
+            {
+                array[i] = random.Next(0, 20);
+            }
+
+            return array;
+        }
+
+        static void ConsoleWriteArray(int[] array)
+        {
+            Console.WriteLine("{");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"[{i}] => {array[i]}");
+            }
+            Console.WriteLine("}\n");
         }
     }
 }
