8810250d897c7f394d9c6b06c2b42aa7b9b98a62
Кирилл Ямщиков <yamshchikov_k@mail.ru>
2022-05-24 19:18:12 +0300

    Add Fibonacci search algorithm.

--- ArraySearch/SearchStrategy/Strategy/FibonacciSearchStrategy.cs
+++ ArraySearch/SearchStrategy/Strategy/FibonacciSearchStrategy.cs
@@ -4,7 +4,40 @@
     {
         public int? Search(int[] array, int elem)
         {
-            return 0;
+            int k = 0, minIndex = Fibonacci(1);
+
+            for(int fib = Fibonacci(k + 1); fib < array.Length; k++)
+            {
+                if(fib < minIndex)
+                    minIndex = fib;
+            }
+
+            int index, offset = 0;
+
+            for (; k > 0; )
+            {
+                index = offset + Fibonacci((--k) + 1);
+
+                if (index >= array.Length || elem < array[index])
+                    continue;
+                else if (elem > array[index])
+                {
+                    offset = index;
+                    k--;
+                }
+                else
+                    return index;
+            }
+
+            return null;
+        }
+
+        private int Fibonacci(int n)
+        {
+            if (n == 0 || n == 1)
+                return n;
+
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
         }
 
         public override string ToString() => "Поиск Фибоначчи";
