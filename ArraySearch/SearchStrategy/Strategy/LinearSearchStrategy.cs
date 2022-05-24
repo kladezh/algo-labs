5bd749890476ceb650b12700c42717c0265f146c
Кирилл Ямщиков <yamshchikov_k@mail.ru>
2022-05-24 18:56:57 +0300

    Complete linear search.

--- ArraySearch/SearchStrategy/Strategy/LinearSearchStrategy.cs
+++ ArraySearch/SearchStrategy/Strategy/LinearSearchStrategy.cs
@@ -4,7 +4,13 @@
     {
         public int? Search(int[] array, int elem)
         {
-            return 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == elem)
+                    return i;
+            }
+
+            return null;
         }
 
         public override string ToString() => "Линейный Поиск";
